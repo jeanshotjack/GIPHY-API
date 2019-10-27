28769443eb8ae98da059a2c7e09448a6532c6762
jeanshotjack <dshalenkova@gmail.com>
2019-10-27 15:27:11 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// topics array for gifs
+// button for each string in array
+var topics = [];
+
+function displayButton () {
+    $("#gifButton").empty(); // empty div prevent duplicate
+    for (var i=0; i < topics.length; i++) {
+        // create button 
+        // add classadd attribute data topic[i]
+        // text topics i;
+        // append button to gifButton
+    }
+}
+
