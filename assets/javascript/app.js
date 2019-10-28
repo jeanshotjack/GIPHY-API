e6a3a283d3098e40027617877640eb967fb29d77
jeanshotjack <dshalenkova@gmail.com>
2019-10-28 19:34:18 -0400

    make divs and add new function but it doesn't work for some reason

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,55 @@
-$(document).ready(function () {
     // topics array for gifs
     
-    var topics = ["judgment", "sarcasm", "laughing", "crying", "surprised", "no", "yes", "clapping"];
+    var topics = ["judgment", "sarcasm", "laugh", "cry", "surprised", "no", "yes", "clapping", "anger", "excitement"];
 
     // button for each string in array
-    function reactButtons() {
-        $("#gifClick").empty(); // empty div prevent duplicate
-        for (var i = 0; i < topics.length; i++) {
+    function makeButtons() {
+        for (i = 0; i < topics.length; i++) {
             // create button 
-            var gifClick = ("<button>");
-            gifClick.addClass("topic");
+            var react = $("<button>");
+            react.addClass("topic");
             // add class
             //add attribute
-            gifClick.attr("data-name", topics[i]);
-            gifClick.text(topics[i]);
-            $("#gifClick").append(gifClick);
+            react.attr("data-name", topics[i]);
+            react.text(topics[i]);
+            $("#gifClick").append(react);
         }
     }
-});
+
+    // make another button for whatever user writes in
+    $("#add-react").on("click", function(event) {
+        // so it can't submit on its own
+        event.preventDefault();
+        var react = $("#react-input").val().trim();
+        // The movie from the textbox is then added to our array
+        topics.push(react);
+        makeButtons();
+      });
+
+      // display the whole thing
+      makeButtons();
+
+// NOTES
+
+// TO PAUSE GIFS 
+/* <img src="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-still="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif" data-animate="https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif" data-state="still" class="gif">
+  <img src="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif" data-still="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif" data-animate="https://media2.giphy.com/media/8rFQp4kHXJ0gU/200.gif" data-state="still" class="gif">
+  <img src="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif" data-still="https://media3.giphy.com/media/W6LbnBigDe4ZG/200_s.gif" data-animate="https://media3.giphy.com/media/W6LbnBigDe4ZG/200.gif" data-state="still" class="gif">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script type="text/javascript">
+    $(".gif").on("click", function() { - WHEN YOU CLICK THE IMAGE
+  
+      var state = $(this).attr("data-state"); - STORE IMAGE STATE
+
+      var animate = $(this).attr("data-animate"); - SET VARS FOR STILL STATE AND ANIMATED STATE
+      var still = $(this).attr("data-still");
+
+      if (state === "still") { - IT WILL ANIMATE IF STILL
+        $(this).attr("src", animate);
+        $(this).attr("data-state", "animate");
+      }
+      if (state === "animate") { - IT WILL PAUSE IF MOVING
+        $(this).attr("src", still);
+        $(this).attr("data-state", "still");
+      } */
 
